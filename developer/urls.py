b84acabeadd9957b47b1c794752fd48a376c6a69
Dao Hong <mmtsd3333@gmail.com>
2025-05-09 21:48:58 +0900

    fix: developer app template url 오류 수정

--- developer/urls.py
+++ developer/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'developer'
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='index'),
     path('docs-hub/', views.docs_hub, name='docs-hub'),
     path('api-client-docs/', views.api_client_docs, name='api-client-docs'),
     path('frontend-guide/', views.frontend_guide, name='frontend-guide'),
